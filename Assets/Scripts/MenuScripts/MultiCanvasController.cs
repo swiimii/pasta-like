eccea58ebd003f03b7a067cb6471f3b9f0ad5415
Samuel Scherer <scheresr@mail.uc.edu>
2020-02-22 14:03:45 -0500

    Main Menu Scene

---
+++ Assets/Scripts/MenuScripts/MultiCanvasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiCanvasController : MonoBehaviour
+{
+    // Switch from canvas 1 to canvas 2
+    public GameObject[] menus;
+    public void SwitchCanvas(int can)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if (i == can)
+            {
+                menus[i].SetActive(true);
+            }
+            else
+            {
+                menus[i].SetActive(false);
+            }
+        }
+    }
+}
