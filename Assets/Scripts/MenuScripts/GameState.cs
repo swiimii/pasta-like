aa020c660b7ba84b0344ee9be419c0aa0d7e741f
Samuel Scherer <scheresr@mail.uc.edu>
2020-02-22 19:19:02 -0500

    Added player lobby mock stuff

---
+++ Assets/Scripts/MenuScripts/GameState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [SerializeField] List<string> players;
+
+    private void Start()
+    {
+        if(GameObject.FindGameObjectsWithTag("GameController").Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddPlayer(string playerName)
+    {
+        players.Add(playerName);
+    }
+
+    public void ResetPlayers()
+    {
+        players.Clear();
+    }
+
+    public int PlayerCount()
+    {
+        return players.Count;
+    }
+}
---
+++ Assets/Scripts/MenuScripts/LobbyPlayerList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LobbyPlayerList : MonoBehaviour
+{
+    [SerializeField] Text playerList;
+    int maxPlayers = 4;
+
+    private void OnEnable()
+    {
+        StartCoroutine("MockPlayersJoining");
+    }
+
+    public void AddPlayerToList(string playerName)
+    {
+        var state = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameState>();
+        if (state.PlayerCount() <= maxPlayers)
+        {
+            
+            if (!playerList.text.Equals(""))
+            {
+                playerList.text += "\n";
+            }
+            playerList.text += playerName;
+            state.AddPlayer(playerName);
+        }
+    }
+
+    public void ResetPlayerList()
+    {
+        playerList.text = "";
+        var state = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameState>();
+        state.ResetPlayers();
+
+    }
+
+    public IEnumerator MockPlayersJoining()
+    {
+        while(true)
+        {
+            AddPlayerToList(name);
+            yield return new WaitForSeconds(4);
+        }
+    }
+
+}
